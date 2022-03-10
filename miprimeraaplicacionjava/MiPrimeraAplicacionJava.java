34c45c1f42f8b7f3b52ce4280a1ab33418ca9eab
Nestor <orellananestor37@gmail.com>
2022-03-10 09:51:01 -0600

    Initial commit

---
+++ miprimeraaplicacionjava/MiPrimeraAplicacionJava.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraaplicacionjava;
+
+/**
+ *
+ * @author ITCA
+ */
+public class MiPrimeraAplicacionJava {
+
+    /**
+     
+     */
+    public static void main(String[] args) {
+        // Declarar variables (ejemplos de algunos tipos de datos)
+        String NombreCOmpleto,Direccion,estadocivil;
+        String dir = "San Salvador";
+        int edad;
+        double gastosdiarios;
+        char genero;
+        boolean poseenVehiculo;
+        //Inicializacion de variables
+        nombrecompleto = "Camila Esperanza Rooslers";
+        Direccion = "Calle de la amagura desvio la tristeza";
+        estadocivil = "Soltera";
+        edad = 30;
+        Gastosdiarios = 34,45;
+        genero = "F";
+        poseevehiculo = "False";
+        
+        //Tipo de variables (Mostrar en pantalla las variables)
+        System,out.println("Nombre completo: " + nombreCompleto);
+        System,out.println
+                
+                
+        
+    }
+    
+}
