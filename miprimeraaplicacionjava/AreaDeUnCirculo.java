fa0b2f6730caec99d5d0e60e5eb5bdd8c3ee580e
Nestor <orellananestor37@gmail.com>
2022-03-10 11:13:18 -0600

    CalculoDeInteresSimple
    Trabajo de Practica Realizado

---
+++ miprimeraaplicacionjava/AreaDeUnCirculo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraaplicacionjava;
+//Importar las clases para el formato
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+/**
+ *
+ * @author ITCA
+ */
+public class AreaDeUnCirculo {
+    public static void main(String[] args) {
+        //Declarar variables
+        double area , radio;
+        final double pi = 3.1416; //la palabra final indica que ese valor de la variable no cambiara
+        //Inicializacion de variables
+        area = 145;
+        //Uso de variables
+        radio = Match.sqrt (area/pi): //Calcular raiz cuadrada con la funcion Macth.sqrt
+        System.out.println("Radio: " + radio + radio + " cm ");
+        
+    }
+    
+}
---
+++ miprimeraaplicacionjava/CalculoInteresSimple.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraaplicacionjava;
+
+/**
+ *
+ * @author ITCA
+ */
+public class CalculoInteresSimple {
+    public static void main(String[] args) {
+      //Decclarar variables
+      double interes , capital prestado ,tiempo, tasadeinteres , TiempoEnMesa;
+      
+      //Inicializar variables
+      interes = 144;
+      capitalPrestado = 1200.00;
+      tasaDeInteres = 0.08;
+      
+      //Uso de variables
+      tiempo = interes / ( capitalPrestado * tasa de interes);
+      tiempoEnMesa = tiempo * 12;
+      System.out.println("Tiempo: " + tiempo +" años");
+      System.out.println("tiempo: " + tiempoEnMesa + "meses");
+      
+        
+    }
+    
+}
